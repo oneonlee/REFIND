eab228d21a8439331f9a70e388fd050b08210471
oneonlee <dg.lee@postech.ac.kr>
2024-12-19 16:09:35 +0900

    rename baseline_model to XLM-R

--- scripts/run_XLM-R.sh
+++ scripts/run_XLM-R.sh
@@ -11,9 +11,9 @@ model_checkpoint="$output_path/checkpoint-145"
 ### Using the baseline model script
 
 # To train a model using the validation dataset excluding one test language:
-python model/baseline_model.py --mode train --model_checkpoint $output_path --data_path $data_path --pred_path $pred_path --test_lang $test_lang
+python model/XLM-R.py --mode train --model_checkpoint $output_path --data_path $data_path --pred_path $pred_path --test_lang $test_lang
 
 # To get predictions from a trained model:
-python model/baseline_model.py --mode test --model_checkpoint "$model_checkpoint" --data_path $data_path --pred_path $pred_path --test_lang $test_lang
+python model/XLM-R.py --mode test --model_checkpoint "$model_checkpoint" --data_path $data_path --pred_path $pred_path --test_lang $test_lang
 
 # Note: The soft label and hard label predictions will be written to JSON files.
